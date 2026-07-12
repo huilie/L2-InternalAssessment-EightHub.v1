43417d64257251082efa5543c20dffd8ee9020b3
huile <lhui64373@gmail.com>
2026-07-12 22:24:58 +1200

    finish account create

--- database.py
+++ database.py
@@ -2,16 +2,21 @@ from imports import *
 
 '''link the database and python files'''
 
-account_p_connection = sqlite3.connect("database/account_password.db")
+account_p_connection = sqlite3.connect(
+    "database/account_password.db",
+    check_same_thread=False # 检查flask多请求
+)
 
 post_recourse_eid_connection = sqlite3.connect(
-    "database/post_resources_p_eid.db"
+    "database/post_resources_p_eid.db",
+    check_same_thread=False
 )
 recourse_eid_connection = sqlite3.connect(
-    "database/resources_eid.db"
+    "database/resources_eid.db",
+    check_same_thread=False
 )
 
-cursor = account_p_connection.sursor()
+cursor = account_p_connection.cursor()
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from imports import *
 
-
+app.secret_key = "awdawdawd"
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -8,53 +8,36 @@ def index():
 
 @app.route("/account_create", methods=['GET', 'POST'])
 def account_create():
-    # if request.method == "POST":
-    #     if 'account-name' in request.form:
     if request.method == "POST":
         if 'account_name' in request.form:
             input_name = request.form.get('account_name')
-            input_password = request.form.get('account_password')
-            conaccounts = sqlite3.connect('database/account_password.db') # 将指定的数据库连接到 Python 变量
-            accountscursor = conaccounts.cursor() # 将 Python 变量指定为数据库的“游标”
-            accountscursor.execute(
-                "INSERT INTO accountinfo (accountemail, accountpassword) VALUES(?,?)", 
-                (input_name, input_password) 
-            )
-            conaccounts.commit()
-            conaccounts.close()
-
-#加一个使用“-”的命名错误并改为“_”
-
-
-        return redirect(url_for('account_create_password'))
+            session["input_name"] = input_name  # 把变量 email 的值保存起来，并命名为 "email"，以后这个用户访问其他页面时都可以取出来
+            return redirect(url_for('account_create_password'))
+        # 将变量提前储存
     return render_template("account_create.html")
 
-@app.route("/account_create-password")
+
+@app.route("/account_create-password", methods=['GET', 'POST'])
 def account_create_password():
     if request.method == "POST":
         if 'account_password' in request.form:
             input_password = request.form.get('account_password')
+            input_name = session.get("input_name")
             conaccounts = sqlite3.connect('database/account_password.db')
             accountscursor = conaccounts.suror()
             accountscursor.execute(
                 "INSERT INTO accountinfo (accountemail, accountpassword) VALUES(?,?)", 
-                (input_password) 
+                (input_name, input_password) 
             )
             
             conaccounts.commit()
             conaccounts.close()
-
-
     return render_template('account_create_password.html')
 
 
 @app.route("/homepage")
 def homepage():
     return render_template("homepage.html")
-
-# {asdf--
-
-
 
 @app.route("/resources")
 def resources():
